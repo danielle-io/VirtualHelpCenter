ade3c7429e324f38ec62b9855779c826542efef7
Melayna Navarro <mjnavarro0089@gmail.com>
2020-04-15 16:41:45 -0700

    time submitted

--- pages/ticketUI.js
+++ pages/ticketUI.js
@@ -3,12 +3,13 @@ import axios from "~/plugins/axios";
 
 axios.post('/api/insertTicket',
 {
-    status: 'Open',
+    status: 'Closed',
     codeSnippet: '<h1>testing this out<h1>',
     questions: ['Ive got a question'],
     attachments: ['attachment1'],
     oneLineOverview: 'im having issues',
     longerDescription: 'seriously j+1 doesnt works',
     owner: 'Johnny John',
-    acceptedBy: 'General Pattis'
+    acceptedBy: 'General Pattis',
+    timeSubmitted: 'String'
 });
