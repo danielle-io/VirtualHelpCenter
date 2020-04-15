c36808eb9829d7ee9c8683f73ca3205e0693b2b4
Danielle Muhlenberg <dmuhlenb@uci.edu>
2020-04-15 00:03:31 -0700

    worked with Melayna to parse over tickets in db and display them with some of the relevant info

--- pages/listTickets.vue
+++ pages/listTickets.vue
@@ -1,53 +1,113 @@
 <template>
   <section class="container">
-    <h1 class="title">
-      Home
-    </h1>
-    <h2 class="info">
-      {{ ticket.owner }}
-    </h2>
-    <nuxt-link class="button" to="/testing/user_demo">
-      Users
-    </nuxt-link>
+    <div class="topElement">
+      <h1 class="title">Ticket Display</h1>
+      <!-- <ul class="users"> -->
+      <div v-for="(thing, index) in tickets" :key="index" class="tickets">
+        <md-card>
+          <md-card-header>
+            <div class="md-title">Ticket</div>
+          </md-card-header>
+
+          <div class="md-card-content">
+            <strong>Student:</strong>
+            {{ thing.owner }}
+          </div>
+
+          <div class="md-card-content">
+            <strong>Status:</strong>
+            {{ thing.status }}
+          </div>
+          <div class="md-card-content">
+            <strong>Questions:</strong>
+          </div>
+
+          <div
+            class="md-card-content"
+            style="margin-left: 25px;"
+            v-for="(question, index) in thing.questions"
+            :key="index"
+          >
+            <strong>{{index + 1}}.</strong>
+            {{ thing.questions[index] }}
+          </div>
+
+          <!-- <div class="md-subhead">Questions: {{ thing.questions }}</div> -->
+        </md-card>
+
+        <!-- <nuxt-link :to="{ status: 'id', params: { ststus: index }}">{{ user.name }}</nuxt-link> -->
+      </div>
+      <!-- </ul> -->
+    </div>
   </section>
 </template>
 
 <script>
-import axios from '~/plugins/axios'
+import Vue from "vue";
+import axios from "~/plugins/axios";
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
 
 export default {
-  name: 'id',
-  asyncData ({ params, error }) {
-    return axios.get('/api/tickets/')
-      .then((res) => {
-        return { tickets: res.data }
-      })
-      .catch((e) => {
-        error({ statusCode: 404, message: 'User not found' })
-      })
+  async asyncData() {
+    let { data } = await axios.get("/api/tickets");
+    return { tickets: data };
   },
-  head () {
+  head() {
     return {
-    //   title: `Ticket: ${this.ticket.owner}`
-    }
+      title: "Tickets"
+    };
   }
-}
+};
 </script>
 
 <style scoped>
-.title
-{
+.container {
+  margin: 0;
+  width: 100%;
+  padding: 100px 0;
+  text-align: center;
+}
+.title {
+  margin: 30px 0;
+}
+.users {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.user {
+  margin: 10px 0;
+}
+.md-subhead {
+  justify-content: left;
+}
+
+.md-card-content {
+  text-align: left;
+  line-height: 2px;
+}
+.md-card {
+  width: 250px;
+  margin: 14px;
+  display: inline-block;
+  vertical-align: top;
+}
+
+.card-row {
+  display: inline-block;
+}
+.title {
   margin-top: 30px;
 }
-.info
-{
+.info {
   font-weight: 300;
   color: #9aabb1;
   margin: 0;
   margin-top: 10px;
 }
-.button
-{
+.button {
   margin-top: 30px;
 }
 </style>
