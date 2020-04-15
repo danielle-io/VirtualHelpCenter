b695aca7e3c8dc9d5991b1b7440742bdbe21c642
Danielle Muhlenberg <dmuhlenb@uci.edu>
2020-04-14 22:11:30 -0700

    reading into vue

---
+++ pages/listTickets.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="container">
+    <h1 class="title">
+      Home
+    </h1>
+    <h2 class="info">
+      {{ ticket.owner }}
+    </h2>
+    <nuxt-link class="button" to="/testing/user_demo">
+      Users
+    </nuxt-link>
+  </section>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+
+export default {
+  name: 'id',
+  asyncData ({ params, error }) {
+    return axios.get('/api/tickets/')
+      .then((res) => {
+        return { tickets: res.data }
+      })
+      .catch((e) => {
+        error({ statusCode: 404, message: 'User not found' })
+      })
+  },
+  head () {
+    return {
+    //   title: `Ticket: ${this.ticket.owner}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title
+{
+  margin-top: 30px;
+}
+.info
+{
+  font-weight: 300;
+  color: #9aabb1;
+  margin: 0;
+  margin-top: 10px;
+}
+.button
+{
+  margin-top: 30px;
+}
+</style>
