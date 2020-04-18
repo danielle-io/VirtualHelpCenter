5618876d2a090d2b7409443fad1eaeae82356a25
Danielle Muhlenberg <dmuhlenb@uci.edu>
2020-04-18 09:58:46 -0700

    nothing

--- pages/ticketUI.js
+++ pages/ticketUI.js
@@ -4,12 +4,12 @@ import axios from "~/plugins/axios";
 axios.post('/api/insertTicket',
 {
     status: 'In Progress',
-    codeSnippet: '<h1>testing this out</h1>',
-    questions: ['Ive got a question'],
+    codeSnippet: '<h1 heAding</h1>',
+    questions: ['what is wrong with my heading'],
     attachments: ['attachment1'],
     oneLineOverview: 'im having issues',
     longerDescription: 'seriously j+1 doesnt works',
-    owner: 'Robin Hood',
-    acceptedBy: 'General Pattis'
+    owner: 'Apple Kay',
+    acceptedBy: 'Jimmy Lin'
 });
 
