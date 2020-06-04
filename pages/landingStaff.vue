b5791bc0549fe0284d0fbb415e9acce651d8e66c
israeljimenez <israelj@uci.edu>
2020-06-03 23:32:50 -0700

    fixed currentTicket date and time

--- pages/landingStaff.vue
+++ pages/landingStaff.vue
@@ -1,9 +1,4 @@
 <style scoped>
-.CodeMirror {
-  float: left;
-  width: 700px;
-  border: 1px solid rgb(230, 221, 221);
-}
 .material-design-icon {
   margin-right: 10px !important;
   margin-top: 3px;
@@ -467,22 +462,6 @@ button[type="submit"] {
                           </span>
                         </div>
                       </div>
-
-                      <div v-if="ticket.codeSnippet !== ''">
-                        <div class="card-line">
-                          <div class="row">
-                            <span class="ticket-categories col-sm-3">
-                              <code-symbol />
-                              <strong>Code:</strong>
-                            </span>
-                            <!-- <span style="padding-left: 20px !important;" class="col-sm-9 text-body"> -->
-                            <Codemirror
-                              v-bind:initialCode="ticket.codeSnippet"
-                              v-model="ticket.codeSnippet"
-                            />
-                          </div>
-                        </div>
-                      </div>
                     </div>
                   </md-card>
                 </div>
@@ -590,7 +569,7 @@ button[type="submit"] {
                     </div>
 
                     <div v-if="ticket.attachments.length > 0">
-                      <div class="card-line-history">
+                      <div class="card-line">
                         <span class="row">
                           <span class="card-categories col-sm-3">
                             <attachment class="label-icons" />Files:
@@ -612,22 +591,6 @@ button[type="submit"] {
                             </a>
                           </span>
                         </span>
-                      </div>
-                    </div>
-
-                    <div v-if="ticket.codeSnippet !== ''">
-                      <div class="card-line-history">
-                        <div class="row">
-                          <span class="card-categories col-sm-3">
-                            <code-symbol class="label-icons" />Code:
-                          </span>
-                          <Codemirror
-                            v-bind:initialCode="ticket.codeSnippet"
-                            v-model="ticket.codeSnippet"
-                          />
-
-                          <!-- </span> -->
-                        </div>
                       </div>
                     </div>
                   </div>
@@ -668,7 +631,7 @@ button[type="submit"] {
                     </span>
                     <span
                       class="col-sm-9 text-body"
-                    >{{ ticket.updatedAt.toLocaleDateString().replace('/','-').replace('/','-')}}</span>
+                    >{{currentTicket.updatedAt.toLocaleDateString().replace('/','-').replace('/','-')}}</span>
                   </div>
                 </div>
 
@@ -679,7 +642,7 @@ button[type="submit"] {
                     </span>
                     <span
                       class="col-sm-9 text-body"
-                    >{{ ticket.updatedAt.toLocaleTimeString()}}</span>
+                    >{{ currentTicket.updatedAt.toLocaleTimeString()}}</span>
                   </div>
                 </div>
 
@@ -769,7 +732,6 @@ import { BFormInput, BFormSelect, BFormTextarea, BButton } from "bootstrap-vue";
 import VueMaterial from "vue-material";
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
-import Codemirror from "../components/Codemirror";
 
 import Ticket from "../ui/models/Ticket";
 
@@ -790,8 +752,8 @@ export default {
     "b-form-select": BFormSelect,
     "b-button": BButton,
     "b-form-input": BFormInput,
-    "b-form-select": BFormSelect,
-    Codemirror: Codemirror
+    "b-form-select": BFormSelect
+    // codemirror
   },
   data() {
     return {
